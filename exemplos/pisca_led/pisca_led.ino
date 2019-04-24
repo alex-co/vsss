c710edc1fbbcf646aa4d7135e1376883fc2cc7c5
alex-co <alex-co@users.noreply.github.com>
2019-04-23 21:09:10 -0300

    Update pisca_led.ino

--- exemplos/pisca_led/pisca_led.ino
+++ exemplos/pisca_led/pisca_led.ino
@@ -6,8 +6,8 @@
 *  - LED piscando com ciclos de 0.8s, 1.6s, 3.2s, 4s e 8s.
 *    A contagem dos ciclos devem ser transmitida via interface serial.
 * 
-* 	Objetivo: Trabalhar com multiplicações e divisões por potências 
-* 				de 2 usando deslocamentos de bits.
+*    Objetivo: Trabalhar com multiplicações e divisões por potências 
+*              de 2 usando deslocamentos de bits.
 *
 */
 /* ****************************************************************** */
@@ -124,7 +124,7 @@ void setup() {
 void loop() {
     
     // ******************************************************* //
-	// Tarefas que devem ser executadas em intervalos de 10ms
+    // Tarefas que devem ser executadas em intervalos de 10ms
     if( (millis() - tasks.last_10ms) > 10 ){
         tasks.last_10ms = millis();
 
@@ -136,37 +136,37 @@ void loop() {
     if( (millis() - tasks.last_100ms) > 100 ){
         tasks.last_100ms = millis();
 
-		// Conta número de eventos de 100ms
-		#if  defined(LED_08S) || defined(LED_16S) || defined(LED_32S)
+	// Conta número de eventos de 100ms
+	#if  defined(LED_08S) || defined(LED_16S) || defined(LED_32S)
         blinker++;
         #endif
         
-    // Pisca o LED com ciclo de 0.8s
-    #ifdef LED_08S
-    digitalWrite(LED, bitRead(blinker, 2));
-    if( !(blinker & B111) ){
-      counter++;
-      Serial.println(counter);
-    }
+    	// Pisca o LED com ciclo de 0.8s
+    	#ifdef LED_08S
+        digitalWrite(LED, bitRead(blinker, 2));
+        if( !(blinker & B111) ){
+            counter++;
+            Serial.println(counter);
+        }
         #endif
         
-    // Pisca o LED com ciclo de 1.6s
-		#ifdef LED_16S
-		digitalWrite(LED, bitRead(blinker, 3));
-		if( !(blinker & B1111) ){     // Incrementa contador e incrementa contador quando
-      counter++;                  // os quatro últimos bits de 'blinker' forem zero.
-		  Serial.println(counter);
-		}
-		#endif
+        // Pisca o LED com ciclo de 1.6s
+        #ifdef LED_16S
+        digitalWrite(LED, bitRead(blinker, 3));
+        if( !(blinker & B1111) ){       // Incrementa contador e incrementa contador quando
+            counter++;                  // os quatro últimos bits de 'blinker' forem zero.
+            Serial.println(counter);
+        }
+	#endif
 
-		// Pisca o LED com ciclo de 3.2s
-    #ifdef LED_32S
-    digitalWrite(LED, bitRead(blinker, 4));
-    if( !(blinker & B11111) ){
-      counter++;
-      Serial.println(counter);
-    }
-		#endif
+       // Pisca o LED com ciclo de 3.2s
+       #ifdef LED_32S
+       digitalWrite(LED, bitRead(blinker, 4));
+       if( !(blinker & B11111) ){
+           counter++;
+           Serial.println(counter);
+       }
+       #endif
 
 
     }
@@ -175,40 +175,40 @@ void loop() {
     if( (millis() - tasks.last_1000ms) > 1000 ){
         tasks.last_1000ms = millis();
 
-		// Conta número de eventos de 1000ms
-		#if  defined(LED_20S) || defined(LED_40S) || defined(LED_80S)
-    blinker++;
-    #endif
+        // Conta número de eventos de 1000ms
+	#if  defined(LED_20S) || defined(LED_40S) || defined(LED_80S)
+        blinker++;
+        #endif
         
-    // Pisca o LED com ciclo de 2s
-		#ifdef LED_20S
-    digitalWrite(LED, bitRead(blinker, 0));
-    if( !(blinker & B1) ){
-      counter++;
-      Serial.println(counter);
-    }
-    #endif
+        // Pisca o LED com ciclo de 2s
+        #ifdef LED_20S
+        digitalWrite(LED, bitRead(blinker, 0));
+        if( !(blinker & B1) ){
+            counter++;
+            Serial.println(counter);
+        }
+        #endif
 
-    // Pisca o LED com ciclo de 4s
-		#ifdef LED_40S
-    digitalWrite(LED, bitRead(blinker, 1));
-    if( !(blinker & B11) ){
-      counter++;
-      Serial.println(counter);
-    }
-    #endif
+       // Pisca o LED com ciclo de 4s
+       #ifdef LED_40S
+       digitalWrite(LED, bitRead(blinker, 1));
+       if( !(blinker & B11) ){
+           counter++;
+           Serial.println(counter);
+       }
+       #endif
 
-    // Pisca o LED com ciclo de 8s
-		#ifdef LED_80S
-    digitalWrite(LED, bitRead(blinker, 2));
-    if( !(blinker & B111) ){
-      counter++;
-      Serial.println(counter);
-    }
-		#endif
+       // Pisca o LED com ciclo de 8s
+       #ifdef LED_80S
+       digitalWrite(LED, bitRead(blinker, 2));
+       if( !(blinker & B111) ){
+           counter++;
+           Serial.println(counter);
+        }
+        #endif
     
     }
-	// ******************************************************* //
+    // ******************************************************* //
 }
 
 
