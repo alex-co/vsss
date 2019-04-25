d85c760401772d2cc95c8c1f25f4e92d9b733cb9
Alexandre <alexandre.co@gmail.com>
2019-04-25 16:07:56 -0300

    Update exemplos/

--- exemplos/end_radio/end_radio.ino
+++ exemplos/end_radio/end_radio.ino
@@ -1,36 +1,28 @@
 /*
-*	Exemplo referente as questões 1.1a e 1.1b de Atividades.txt
+*	Exemplo referente a questão 1.2 de Atividades.txt
 * 
-*  - Escalonador sem chamadas de funções.
-*
-*  - LED piscando com ciclos de 0.8s, 1.6s, 3.2s, 4s e 8s.
-*    A contagem dos ciclos devem ser transmitida via interface serial.
+*  - Lê o endereço de rádio, definido pelo dipswitch
 * 
-*    Objetivo: Trabalhar com multiplicações e divisões por potências 
-*              de 2 usando deslocamentos de bits.
+*    Objetivo: Trabalhar com pinos individuais de I/O.
 *
 */
 /* ****************************************************************** */
 
+#include <avr/io.h>
 #include <avr/pgmspace.h>
 
 /* ****************************************************************** */
 /* *** Definições diversas ****************************************** */
 
 // =================================================================== //
-// Ref Ex 1.1b - Descomentar somente uma das definições abaixo por vez:
+// Ref Ex 1.2 - Descomentar somente uma das definições abaixo por vez:
 
-#define LED_08S		// LED pisca com ciclo de 0.8s
-//#define LED_16S		// LED pisca com ciclo de 1.6s
-//#define LED_32S		// LED pisca com ciclo de 3.2s
-//#define LED_20S		// LED pisca com ciclo de 2.0s
-//#define LED_40S		// LED pisca com ciclo de 4.0s
-//#define LED_80S		// LED pisca com ciclo de 8.0s
+//#define RD_PINS		// Lê pinos de I/O individualmente
+#define RD_PORT		// Lê PORTC ( PINC ) do Atmega328
 
 // =================================================================== //
 
 #define SSPEED        115200   // Velocidade da interface serial
-
 
 /* ****************************************************************** */
 /* Estas são as conexões de hardware mapeadas aos pinos do Arduino ** */
@@ -72,12 +64,11 @@ typedef struct {
 
 TasksTCtr tasks;		// Contagem de tempo para execução de tarefas
 
-uint32_t counter;		
-uint32_t blinker;		
-
 
 /* ******************************************************************* */
 /* *** Protótipos das funções **************************************** */
+
+uint8_t get_node_addr( void );
 
 
 /* ******************************************************************* */
@@ -85,14 +76,12 @@ uint32_t blinker;
 
 void setup() {
 
-    Serial.begin(115200);               // Inicialização da com. serial
-
-    // Inicialização do pino do LED
-    pinMode(LED, OUTPUT);               // Pino do LED como saída digital
-    digitalWrite(LED, LOW);
+    Serial.begin(115200);   // Inicialização da com. serial
     
-    counter = 0;
-    blinker = 0;   
+    // Inicialização dos pinos de endereçamento do rádio
+    pinMode(RADIO_A0, INPUT_PULLUP);    // Endereço deste nó: bit 0
+    pinMode(RADIO_A1, INPUT_PULLUP);    // Endereço deste nó: bit 1
+    
 }
 
 
@@ -108,43 +97,14 @@ void loop() {
 
 
 
+
     }
     // ******************************************************* //
     // Tarefas que devem ser executadas em intervalos de 100ms
     if( (millis() - tasks.last_100ms) > 100 ){
         tasks.last_100ms = millis();
 
-		// Conta número de eventos de 100ms
-		#if  defined(LED_08S) || defined(LED_16S) || defined(LED_32S)
-        blinker++;
-        #endif
-        
-    	// Pisca o LED com ciclo de 0.8s
-    	#ifdef LED_08S
-        digitalWrite(LED, bitRead(blinker, 2));
-        if( !(blinker & B111) ){
-            counter++;
-            Serial.println(counter);
-        }
-        #endif
-        
-        // Pisca o LED com ciclo de 1.6s
-        #ifdef LED_16S
-        digitalWrite(LED, bitRead(blinker, 3));
-        if( !(blinker & B1111) ){       // Incrementa contador e incrementa contador quando
-            counter++;                  // os quatro últimos bits de 'blinker' forem zero.
-            Serial.println(counter);
-        }
-		#endif
 
-       // Pisca o LED com ciclo de 3.2s
-       #ifdef LED_32S
-       digitalWrite(LED, bitRead(blinker, 4));
-       if( !(blinker & B11111) ){
-           counter++;
-           Serial.println(counter);
-       }
-       #endif
 
 
     }
@@ -153,37 +113,9 @@ void loop() {
     if( (millis() - tasks.last_1000ms) > 1000 ){
         tasks.last_1000ms = millis();
 
-        // Conta número de eventos de 1000ms
-		#if  defined(LED_20S) || defined(LED_40S) || defined(LED_80S)
-        blinker++;
-        #endif
-        
-        // Pisca o LED com ciclo de 2s
-        #ifdef LED_20S
-        digitalWrite(LED, bitRead(blinker, 0));
-        if( !(blinker & B1) ){
-            counter++;
-            Serial.println(counter);
-        }
-        #endif
+		// Envia valor do end. de rário via serial
+		Serial.println( get_node_addr() );
 
-       // Pisca o LED com ciclo de 4s
-       #ifdef LED_40S
-       digitalWrite(LED, bitRead(blinker, 1));
-       if( !(blinker & B11) ){
-           counter++;
-           Serial.println(counter);
-       }
-       #endif
-
-       // Pisca o LED com ciclo de 8s
-       #ifdef LED_80S
-       digitalWrite(LED, bitRead(blinker, 2));
-       if( !(blinker & B111) ){
-           counter++;
-           Serial.println(counter);
-        }
-        #endif
     
     }
     // ******************************************************* //
@@ -193,7 +125,31 @@ void loop() {
 /* ******************************************************************* */
 /* *** FUNÇÕES (implementações) ************************************** */
 
-
+uint8_t get_node_addr( void ){
+    
+   
+    // Abordagem: Leitura individual dos pinos
+    #ifdef RD_PINS
+	uint8_t addr = 0xFF;
+    addr  = addr << 1;
+    addr |= digitalRead(RADIO_A1);
+    addr  = addr << 1;
+    addr |= digitalRead(RADIO_A0);
+    addr = ~addr;
+    #endif
+    
+    // Abordagem: Leitura do PORTC (PINC) do uC
+    // RADIO_A0 = Arduino A4 = uC pino 27 = PC4
+    // RADIO_A1 = Arduino A5 = uC pino 28 = PC5
+    #ifdef RD_PORT
+    uint8_t addr = 0;
+	addr |= PINC >> 4;
+	addr |= B11111100;
+	addr  = ~addr;
+    #endif
+	
+	return addr;
+}
 
 /* ****************************************************************** */
 /* ****************************************************************** */
